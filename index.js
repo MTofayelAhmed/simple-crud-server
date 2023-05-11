4ee8a899b83e57668b6cb3ffca381aa2fef21a12
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-05-11 15:57:14 +0600

    crud operation practice

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // const database = client.db("usersDB");
-    // const userCollection = database.collection("users");
-    const userCollection = client.db("usersDB").collection("users");
+    // const database = client.db("userDB")
+    // const userCollection= database.collection("users")
+    const userCollection = client.db("userDB").collection("users");
 
     app.get("/users", async (req, res) => {
       const cursor = userCollection.find();
@@ -35,16 +35,55 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
-      // res.send(user)
-      // console.log("new user", user)
+      console.log(user);
       const result = await userCollection.insertOne(user);
-
-      //   const result = await userCollection.insertOne(user);
-      // res.send(result);
+      console.log(result);
       res.send(result);
     });
+
+    // const database = client.db("usersDB");
+    // const userCollection = database.collection("users");
+    // const userCollection = client.db("usersDB").collection("users");
+
+    // app.post("/users", async (req, res) => {
+    //   const user = req.body;
+    //   // res.send(user)
+    //   // console.log("new user", user)
+    //   const result = await userCollection.insertOne(user);
+
+    //   //   const result = await userCollection.insertOne(user);
+    //   // res.send(result);
+    //   res.send(result);
+    // });
+    app.put(
+      "/users/:id",
+      async(req, res) => {
+        const id = req.params.id;
+        const user = req.body;
+        console.log(user, id);
+        const filter = {_id: new ObjectId(id)}
+        const options = {upsert: true}
+        const updatedUser ={
+          $set: {
+           name: user.name,
+           email: user.email
+          },
+        }
+
+        const result = await userCollection.updateOne(filter, updatedUser, options)
+        res.send(result)
+      })
+
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
